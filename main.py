eaabf098329549935810024f643872762f057ff0
Przemek Dorski <przemek.dorski@o2.pl>
2024-04-16 19:46:12 +0200

    --all

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate  # Import Flask-Migrate
 from datetime import datetime, timedelta
 import requests
-import os  # Dodano import modułu os
+import os
 
 app = Flask(__name__)
-
-# Zaktualizowano konfigurację URI bazy danych do użycia PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql-rugged-54545', 'sqlite:///votes.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///votes.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)  # Inicjalizacja Flask-Migrate
 
 class Vote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    ip_address = db.Column(db.String(45))  # Zaktualizowano rozmiar dla IPv6
+    ip_address = db.Column(db.String(45))  # Updated size for IPv6 compatibility
     user_agent = db.Column(db.String(256))
     country = db.Column(db.String(100))
     city = db.Column(db.String(100))
@@ -41,17 +42,17 @@ def home():
         if recent_vote:
             error = "Możesz głosować tylko raz na 12 godzin z tego samego adresu IP."
         else:
-            token = os.environ.get('IPINFO_TOKEN', '')  # Użycie zmiennej środowiskowej dla tokenu
+            token = os.environ.get('IPINFO_TOKEN', '')
             try:
                 response = requests.get(f'http://ipinfo.io/{ip}?token={token}')
                 response.raise_for_status()
                 ip_data = response.json()
-                country = ip_data.get('country', 'Nieznany')
-                city = ip_data.get('city', 'Nieznane')
+                country = ip_data.get('country', 'Unknown')
+                city = ip_data.get('city', 'Unknown')
             except requests.RequestException as e:
-                error = f'Błąd podczas pobierania informacji o IP: {e}'
-                country = 'Błąd'
-                city = 'Błąd'
+                error = f'Error retrieving IP information: {e}'
+                country = 'Error'
+                city = 'Error'
 
         if not error:
             new_vote = Vote(ip_address=ip, user_agent=user_agent, country=country, city=city,
@@ -64,7 +65,7 @@ def home():
 
 @app.route('/thanks')
 def thanks():
-    return "Dziękujemy za oddanie głosu!"
+    return "Thank you for voting!"
 
 if __name__ == '__main__':
     app.run(debug=True)
